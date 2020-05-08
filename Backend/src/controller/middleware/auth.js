7a9bba523f5032a6a5274c9482a942b35ff6a393
dpq3005 <41153934+dpq3005@users.noreply.github.com>
2020-05-08 18:51:14 +0700

    Update auth.js

--- Backend/src/controller/middleware/auth.js
+++ Backend/src/controller/middleware/auth.js
@@ -3,7 +3,7 @@ const Boom = require('boom');
 
 module.exports = function (req, h, next) {
   //Get token from header
-  const token = req.headers['x-auth-token'];
+  const token = req.headers.authorization;
   //Check if no token 
   if (!token) {
     return h.response('No token');
